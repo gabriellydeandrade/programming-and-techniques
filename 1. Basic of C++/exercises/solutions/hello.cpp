5c66eef5d99ce20a1a6f79d9cefad1a194063f59
Gabrielly de Andrade <gabrielly.andrade@poli.ufrj.br>
2020-05-04 11:47:51 -0300

    refactor: modify structure of project

---
+++ 1. Basic of C++/exercises/solutions/hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream> // lib declaration, iostream
+
+using namespace std;
+
+int main(){
+
+  cout << "Ola Mundo!\n"; // print something in the screen
+  return 0; // flag to indicate the system that the program finish with success
+}
